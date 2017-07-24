155602e78e3ac57b10acfa828fc39afea08fd109
jonharrity <joharrity@gmail.com>
2017-07-23 23:59:33 -0400

    added separate development mode by running startdev.sh

---
+++ productionserver.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile('./index.html').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
